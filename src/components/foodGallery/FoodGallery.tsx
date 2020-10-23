7ea90a7ec22f1bea52a7f9a76ea403a4fe6d251a
Fabio Arcidiacono <fabio.arcidiacono@lastminute.com>
2020-10-23 10:24:06 +0200

    gallery with react components

--- src/components/foodGallery/FoodGallery.tsx
+++ src/components/foodGallery/FoodGallery.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import Carousel, { Modal, ModalGateway } from 'react-images';
+import Gallery, { PhotoProps } from 'react-photo-gallery';
 import { FoodGalleryQuery } from '../../generated/graphql';
 import './styles.css';
 
@@ -6,16 +8,65 @@ interface Props {
   data: FoodGalleryQuery;
 }
 
-const FoodGallery: React.FC<Props> = ({ data }) => (
-  <article className="content">
-    <section className="grid" >
-      {data.food?.gallery?.photos?.map(
-        (photo, i) => (
-          <img key={i} src={photo?.url} alt={photo?.alternativeText || ""} />
-        )
-      )}
-    </section>
-  </article>
-);
+const FoodGallery: React.FC<Props> = ({ data }) => {
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  // .sort(
+  //   (a, b) => {
+  //     if ((a?.url || '') < (b?.url || '')) { return -1; }
+  //     if ((a?.url || '') < (b?.url || '')) { return -1; }
+  //     return 0;
+  //   }
+  // )
+  const photos: PhotoProps[] = data.food?.gallery?.photos?.map(
+    (photo, i) => (
+      {
+        src: photo?.url || '',
+        width: photo?.width || 0,
+        height: photo?.height || 0,
+        alt: photo?.alternativeText || undefined,
+        key: i.toString()
+      }
+    )
+  ) || [
+      {
+        src: '',
+        width: 0,
+        height: 0
+      }
+    ];
+
+  return (
+    <article className="content">
+      <Gallery photos={photos} direction={"column"} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                source: x.src,
+                srcset: x.srcSet
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </article>
+  )
+};
 
 export default FoodGallery
