910711ab1fa1c51255a78f022387e65641a8f9e7
Fabio Arcidiacono <fabio.arcidiacono@lastminute.com>
2020-10-25 19:31:56 +0100

    start working on contact page

--- frontend/src/components/Contact/index.tsx
+++ frontend/src/components/Contact/index.tsx
@@ -6,8 +6,30 @@ interface Props {
   info: ContactInfo;
 }
 
-const Contact: React.FC<Props> = () => (
-  <h1>contacts</h1>
+const Contact: React.FC<Props> = ({ info }) => (
+  <>
+    {info.cover && (
+      <section className="header-image">
+        <img src={info.cover.url} alt="cover" />
+      </section>
+    )}
+    <section className="description">{info.description}</section>
+    <hr />
+    <section className="social">
+      <p>
+        <a href={`mailto:${info.email}`}>
+          <img src="https://noemicilenti.com/img/social/email.png" alt="email" />
+          <span>{info.email}</span>
+        </a>
+      </p>
+      <p>
+        <a href={`tel::${info.phone.replaceAll(' ', '')}`}>
+          <img src="https://noemicilenti.com/img/social/phone.png" alt="phone" />
+          <span>{info.phone}</span>
+        </a>
+      </p>
+    </section>
+  </>
 );
 
 export default Contact;
